d36e3bf7ebe1f7315931eff5a5e25b56b3110704
petersbob <josephpe45@gmail.com>
2015-12-13 23:48:38 -0600

    boolean checks, all 16 sprites

--- Game.js
+++ Game.js
@@ -5,9 +5,9 @@ worldshift.Game = function(game) {
 
 var spriteparts = {
       "head": false,
-      "body": false,
+      "torso": false,
       "arms": false,
-      "legs": true
+      "legs": false
     }
 
 worldshift.Game.prototype = {
@@ -15,21 +15,20 @@ worldshift.Game.prototype = {
 	create: function() {
 		this.world.setBounds(0, 0, 5000, 5000);
 
-		this.bg = this.add.image(0, 0, 'gamebg');
+		//this.bg = this.add.image(0, 0, 'gamebg');
+		//this.layer = this.add.image(0, 0, 'layer1');
 
 		this.physics.startSystem(Phaser.Physics.ARCADE);
 
-		this.sprite = this.add.sprite(0, this.world.centerY, 'chomp');
-		// this.sprite.loadTexture('robot');
 		this.buildsprite();
 
 		this.cursors = this.input.keyboard.createCursorKeys();
 
-
 	},
 
 	buildsprite: function() {
-        
+        //----Character---//
+        this.sprite = this.add.sprite(0, 4900, 'rrrr');
         this.sprite.animations.add('chompstand', [ 9, 10, 9, 8], 3, true);
 		this.sprite.animations.add('chompleft', [0, 1, 2, 3, 4, 5, 6, 7], 10, true);
 		this.sprite.animations.add('chompright', [11, 12, 13, 14, 15, 16, 17, 18], 10, true);
@@ -44,37 +43,110 @@ worldshift.Game.prototype = {
 		this.sprite.body.collideWorldBounds = true;
 		this.sprite.body.bounce.y = 0.2;
 		this.sprite.body.gravity.y = 80;
+		//-------------------//
 
+		//------------body parts-------//
+		this.head = this.add.sprite(300, 4900, 'head');
+		this.physics.enable(this.head, Phaser.Physics.ARCADE);
+		this.head.body.collideWorldBounds = true;
+		this.head.body.bounce.y = 0.2;
+		this.head.body.gravity.y = 80;
+
+		// this.torso = this.add.sprite(300, 4900, 'torso');
+		// this.physics.enable(this.torso, Phaser.Physics.ARCADE);
+		// this.torso.body.collideWorldBounds = true;
+		// this.torso.body.bounce.y = 0.2;
+		// this.torso.body.gravity.y = 80;
+
+		// this.arms = this.add.sprite(300, 4900, 'arms');
+		// this.physics.enable(this.arms, Phaser.Physics.ARCADE);
+		// this.arms.body.collideWorldBounds = true;
+		// this.arms.body.bounce.y = 0.2;
+		// this.arms.body.gravity.y = 80;
+
+		// this.legs = this.add.sprite(300, 4900, 'legs');
+		// this.physics.enable(this.legs, Phaser.Physics.ARCADE);
+		// this.legs.body.collideWorldBounds = true;
+		// this.legs.body.bounce.y = 0.2;
+		// this.legs.body.gravity.y = 80;
+		//-----------------------------//
+
+    },
+
+    checkOverlap: function(spriteA, spriteB) {
+    	var boundsA = spriteA.getBounds();
+    	var boundsB = spriteB.getBounds();
+
+    	return Phaser.Rectangle.intersects(boundsA, boundsB);
     },
 
 	update: function() {
 
-		if (spriteparts["legs"]) {
-			if (this.cursors.right.isDown) {
-				this.sprite.animations.play('chompright');
-				this.sprite.x+=2;
-			}
-			else if (this.cursors.left.isDown) {
-				this.sprite.animations.play('chompleft');
-				this.sprite.x-=2;
-			}
-			else{
-				this.sprite.animations.play('chompstand');
-			}
+		if (this.checkOverlap(this.sprite, this.head)) {
+			console.log("Hit head");
+			this.head.kill();
+			spriteparts["head"] = true;
+			changeBody();
+			// this.sprite.loadTexture('crrr');
+			// this.sprite.body.setSize(112.5, 150);
 		}
-		else {
-			if (this.cursors.right.isDown) {
-				this.sprite.animations.play('robotright');
-				this.sprite.x+=2;
-			}
-			else if (this.cursors.left.isDown) {
-				this.sprite.animations.play('robotleft');
-				this.sprite.x-=2;
-			}
-			else {
-				this.sprite.animations.play('robotstand');
-			}
-		}
+		// if (this.checkOverlap(this.sprite, this.torso)) {
+		// 	console.log("Hit torso");
+		// 	this.torso.kill();
+		// 	spriteparts["torso"] = true;
+		// 	changeBody();
+		// }
+		// if (this.checkOverlap(this.sprite, this.arms)) {
+		// 	console.log("Hit arms");
+		// 	this.arms.kill();
+		// 	spriteparts["arms"]= true;
+		// 	changeBody();
+		// }
+		// if (this.checkOverlap(this.sprite, this.legs)) {
+		// 	console.log("Hit legs");
+		// 	this.legs.kill();
+		// 	spriteparts["legs"] = true;
+		// 	changeBody();
+		// }
+		// if (this.checkOverlap(this.sprite, this.body)) {
+		// 	spriteparts["body"] = true;
+			
+		// }
+		// if (this.checkOverlap(this.sprite, this.arms)) {
+		// 	spriteparts["arms"] = true;
+		// }
+		// if (this.checkOverlap(this.sprite, this.legs)) {
+			
+		// }
+		
+
+
+		// if (spriteparts["head"]) {
+		// 	if (this.cursors.right.isDown) {
+		// 		this.sprite.animations.play('chompright');
+		// 		this.sprite.x+=2;
+		// 	}
+		// 	else if (this.cursors.left.isDown) {
+		// 		this.sprite.animations.play('chompleft');
+		// 		this.sprite.x-=2;
+		// 	}
+		// 	else{
+		// 		this.sprite.animations.play('chompstand');
+		// 	}
+		// }
+		// else {
+		// 	if (this.cursors.right.isDown) {
+		// 		this.sprite.animations.play('robotright');
+		// 		this.sprite.x+=2;
+		// 	}
+		// 	else if (this.cursors.left.isDown) {
+		// 		this.sprite.animations.play('robotleft');
+		// 		this.sprite.x-=2;
+		// 	}
+		// 	else {
+		// 		this.sprite.animations.play('robotstand');
+		// 	}
+		// }
 
 	}
 
@@ -82,4 +154,88 @@ worldshift.Game.prototype = {
 	// 	this.debug.spriteInfo(this.sprite, 40, 100);
  //        this.debug.pointer(this.input.activePointer);
 	// }
+}
+
+
+function changeBody() {
+	if ( spriteparts["head"]) {
+		if (spriteparts["torso"]) {
+
+			if (spriteparts["arms"]) {
+				if (spriteparts["legs"]) {
+					//cccc
+				}
+				else {
+					//cccr
+				}
+			}
+			else {
+				if (spriteparts["legs"]) {
+					//ccrc
+				}
+				else {
+					//ccrr
+				}
+			}
+		}
+		else {
+			if (spriteparts["arms"]) {
+				if (spriteparts["legs"]) {
+					//crcc
+				}
+				else {
+					//crcr
+				}
+			}
+			else {
+				if (spriteparts["legs"]) {
+					//crrc
+				}
+				else {
+					console.log("crrr");
+					//crrr
+				}
+			}
+		}
+	}
+	else {
+		if (spriteparts["torso"]) {
+
+			if (spriteparts["arms"]) {
+				if (spriteparts["legs"]) {
+					//rccc
+				}
+				else {
+					//rccr
+				}
+			}
+			else {
+				if (spriteparts["legs"]) {
+					//rcrc
+				}
+				else {
+					//rcrr
+				}
+			}
+		}
+		else {
+			if (spriteparts["arms"]) {
+				if (spriteparts["legs"]) {
+					//rrcc
+				}
+				else {
+					//rrcr
+				}
+			}
+			else {
+				if (spriteparts["legs"]) {
+					//rrrc
+				}
+				else {
+					//rrrr
+				}
+			}
+		}
+	}
+	// console.log("Hit");
 }
--- Preloader.js
+++ Preloader.js
@@ -18,10 +18,31 @@ worldshift.Preloader.prototype = {
 
 		this.load.image('startbutton', 'images/startbutton.png');
 		this.load.image('gamebg', 'images/Background1-2.png');
+		//---body parts----//
+		this.load.image('head', 'images/diamond.png');
+		this.load.image('torso', 'images/firstaid.png');
+		this.load.image('arms', 'images/star.png');
+		this.load.image('legs', 'images/explosion.png');
+		//------------------//
 
-		this.load.spritesheet('robot', 'images/ROBOTSPRITES.png', 112.5, 113);
-
-		this.load.spritesheet('chomp', 'images/Chompmansprite.png', 112.73, 160);
+		//---------------spritesheets---------------//
+		this.load.spritesheet('rrrr', 'images/playersprites/rrrr.png', 112.5, 113);
+		this.load.spritesheet('rrrc', 'images/playersprites/rrrc.png', 112.73, 160);
+		this.load.spritesheet('rrcr', 'images/playersprites/rrcr.png', 112.73, 160);
+		this.load.spritesheet('rrcc', 'images/playersprites/rrcc.png', 112.73, 160);
+		this.load.spritesheet('rcrr', 'images/playersprites/rcrr.png', 112.73, 160);
+		this.load.spritesheet('rcrc', 'images/playersprites/rcrc.png', 112.73, 160);
+		this.load.spritesheet('rccr', 'images/playersprites/rccr.png', 112.73, 160);
+		this.load.spritesheet('rccc', 'images/playersprites/rccc.png', 112.73, 160);
+		this.load.spritesheet('crrr', 'images/playersprites/crrr.png', 112.73, 160);
+		this.load.spritesheet('crrc', 'images/playersprites/crrc.png', 112.73, 160);
+		this.load.spritesheet('crcr', 'images/playersprites/crcr.png', 112.73, 160);
+		this.load.spritesheet('crcc', 'images/playersprites/crcc.png', 112.5, 150);
+		this.load.spritesheet('rrcc', 'images/playersprites/rrcc.png', 112.73, 160);
+		this.load.spritesheet('ccrc', 'images/playersprites/ccrc.png', 112.73, 160);
+		this.load.spritesheet('cccr', 'images/playersprites/cccr.png', 112.5, 150);
+		this.load.spritesheet('cccc', 'images/playersprites/cccc.png', 112.73, 160);
+		//------------------------------------------//
 
 	},
 
